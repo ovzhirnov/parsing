aba7f6c62cac058282b8a33fa8379c70688e8c4e
Oleg Zhirnov <zovscb@mail.ru>
2020-02-09 18:17:55 +0400

    Fourth commit

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+<meta http-equiv="Content-Type" content="text/html; charset=windows-UTF-8" />
 <meta charset="UTF-8">
 <title>Parsing from parishop.ru</title>
 </head>
 
 <body>
 <?php
-// Третий коммит
 // Автор - Жирнов Олег Вячеславович
 // Парсер цен для url: https://parishop.ru/catalog/zhenskoye-nizhneye-belye/
 // Запись производится в таблицу БД MySQL, параллельно выводится лог на экран
